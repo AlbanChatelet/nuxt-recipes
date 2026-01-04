2a725372a355549dd66b67b6fe7a43b63e0f154f
AlbanChatelet <chateletalban20@gmail.com>
2026-01-04 22:37:57 +0100

    feat(nuxt): add home page with links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <main style="padding:24px;">
+  <main style="padding: 24px;">
     <h1>Nuxt Recipes</h1>
+    <p>Mini front Nuxt connecté à l’API Express + au CMS Sanity.</p>
+
     <ul>
-      <li><NuxtLink to="/recipes">/recipes</NuxtLink></li>
-      <li><NuxtLink to="/books">/books</NuxtLink></li>
+      <li><NuxtLink to="/recipes">Voir les recettes (API)</NuxtLink></li>
+      <li><NuxtLink to="/books">Voir les livres (Sanity)</NuxtLink></li>
     </ul>
   </main>
 </template>
